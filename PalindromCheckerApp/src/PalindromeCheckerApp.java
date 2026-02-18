3daf109df8e33b5afbbb1c4904c365af3a29040c
ayush_12_12 <shivoy002@gmail.com>
2026-02-18 14:30:04 +0530

    UC2 done

--- PalindromCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromCheckerApp/src/PalindromeCheckerApp.java
@@ -32,10 +32,15 @@ public class PalindromeCheckerApp {
      */
     public static void main(String[] args) {
 
-        System.out.println("====================================");
-        System.out.println("  Palindrome Checker Management");
-        System.out.println("====================================");
-        System.out.println("Version: 1.0");
-        System.out.println("Welcome to the application!");
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it Palindrome: " + isPalindrome);
     }
 }
