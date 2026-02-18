045e47012f51d42c94fd8050c3a1073bec938906
ayush_12_12 <shivoy002@gmail.com>
2026-02-18 15:15:36 +0530

    UC4 done

--- PalindromCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromCheckerApp/src/PalindromeCheckerApp.java
@@ -20,6 +20,7 @@
  * @author Developer
  * @version 1.0
  */
+import java.util.Stack;
 public class PalindromeCheckerApp {
 
     /**
@@ -32,23 +33,28 @@ public class PalindromeCheckerApp {
      */
     public static void main(String[] args) {
 
-        String input = "radar";
-        char[] chars = input.toCharArray();
+        // Declare and initialize the input string.
+        String input = "noon";
 
-        int start = 0;
-        int end = chars.length - 1;
+        // Create a Stack to store characters.
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character of the string into the stack.
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Assume palindrome initially.
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        // Iterate again through original string.
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome: " + isPalindrome);
+        System.out.println("Is palindrome: " + isPalindrome);
     }
 }
