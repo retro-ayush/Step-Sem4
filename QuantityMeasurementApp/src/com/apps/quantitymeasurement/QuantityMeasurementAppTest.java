391fec3463c796597b1bbca2d720dabedac087b3
retro-ayush <shivoy002@gmail.com>
2026-04-22 15:39:25 +0530

    Done UC1

---
+++ QuantityMeasurementApp/src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,40 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    public void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    public void testEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    public void testEquality_NonNumericInput() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        String value = "1.0";
+        assertFalse(f1.equals(value));
+    }
+
+    @Test
+    public void testEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        assertTrue(f1.equals(f1));
+    }
+}
